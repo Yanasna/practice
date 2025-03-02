e88cd7b84ea2b89a4a873703dc0b6ade371508c6
Yanasna <116442812+Yanasna@users.noreply.github.com>
2025-03-02 14:31:42 +0900

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class hello {
+	public static int sum(int n, int m) {
+		return n+m;
+	} 
+	
+	public static int avg(int b, int c) {
+		return b*c/2;
+	}
+	
+	 public static void main(String[] args) {
+		int i =20;
+		int s;
+		char a;
+		int f;
+		
+		s = sum(i, 10);
+		a ='?';
+		f = avg(4, 5);
+		System.out.println(s);
+		System.out.println(a);
+		System.out.println(f);
+		System.out.println(3+5);
+		System.out.println("3+5");
+	}
+	
+}
