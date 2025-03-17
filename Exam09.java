5dc024b985bda5a65ef6c0a566d2615d6fd80d63
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-18 00:00:48 +0900

    25.3/18_2

---
+++ Exam09.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Exam09 {
+	public static void main(String[] args) {
+		Book b1 = new Book();
+		Book b2 = new Book("어린왕자", 5000);
+		
+		b1.show_print();
+		b2.show_print();
+	}
+}
