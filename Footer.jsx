1de614006cbd03f6f3c2e9b13bc420fc43989697
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-21 16:41:29 +0900

    3/21_2

--- Footer.jsx
+++ Footer.jsx
@@ -1,9 +1,11 @@
 function Footer() {
     const myName = "김영중";
+    const myPhone = "010-2929-4432";
   return (
     <footer>
         <h1>Footer</h1>
         <h2>내 이름은 {myName} 입니다.</h2>
+        <h2>내 전화번호는 {myPhone} 입니다.</h2>
     </footer>
   );
 }
