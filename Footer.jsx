9d6641b4d18314fd625e13b9500db6e0a61516cd
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-21 16:40:22 +0900

    25/3/21

--- Footer.jsx
+++ Footer.jsx
@@ -1,8 +1,11 @@
-function Footer(){
-    return (
-        <footer>
-            <h1>footer</h1>
-        </footer>
-    );
+function Footer() {
+    const myName = "김영중";
+  return (
+    <footer>
+        <h1>Footer</h1>
+        <h2>내 이름은 {myName} 입니다.</h2>
+    </footer>
+  );
 }
+
 export default Footer;
