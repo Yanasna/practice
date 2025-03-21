ad3c9ef0a60a992c89234165ed3c51f70d40b1cd
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-21 16:42:57 +0900

    3/21_1

--- App.jsx
+++ App.jsx
@@ -1,27 +1,17 @@
-import './App.css'
+import React from 'react'
+import Privacy from './components/Privacy';
 
-function Privacy(props){
-  return (
-    <section>
-      <h2>개인정보</h2>
-      <p>이름 : {props.name}</p>
-      <p>전화번호 : {props.phone}</p>
-      <p>이메일 : {props.email}</p>
-    </section>
-  );
-}
 
 function App() {
   return (
-    <>
-      <h1>이력서</h1>
+    < >
       <Privacy 
-      name = "김영중"
-      phone = "010-2929-4432"
-      email = "hong@gmail.com"
+          name = "김영중"
+          phone = "010-2929-4432 "
+          email = "m604030@naver.com"
       />
     </>
   )
 }
 
-export default App
+export default App;
