e7b252908f1125159952a99aa2f6d9e553b532a3
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-26 11:00:28 +0900

    3/26_2

--- practice.java
+++ practice.java
@@ -10,9 +10,9 @@ public class practice{
 		FileReader fr = new FileReader("C:\\Users\\yura0\\Yellow.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
-		int c;
-		while((c = br.read()) != -1)
-			System.out.print((char)c);   
+		String c;
+		while((c = br.readLine()) != null)
+			System.out.println(c);   
 	
 		
 		System.out.println("무사히 파일 가져오기 성공");
