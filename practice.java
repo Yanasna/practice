caa09528dc6b54663cff7cfae9617480aa48e45b
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-03-26 10:55:16 +0900

    25/3/26_1

--- practice.java
+++ practice.java
@@ -1,51 +1,25 @@
 package practice2;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.io.*;
+import java.util.Scanner;
 
-public class practice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList<User> users = new ArrayList<>();
-		Scanner sc = new Scanner(System.in);
-		while(true) {
-			System.out.println("이름을 입력하세요 >> ");
-			String n = sc.nextLine();
-			if(n.equals("그만"))
-				break;	
-			System.out.println("나이를 입력하세요 >> ");
-			int su = Integer.parseInt(sc.nextLine());
-			
-			User user1 = new User(n, su);
-			
-			users.add(user1);
-			
-			
-		}
-		sc.close();
-		try {
-			FileWriter fw = new FileWriter("C:\\Users\\yura0\\test1.txt");
-			BufferedWriter bw = new BufferedWriter(fw);
-
-			
-			
-			for(User user : users) {
-				bw.write(user.toString());
-				bw.newLine();
-			}
-			
-			bw.close();
-			fw.close();
-			System.out.println("파일 생성 완료!!");
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("오류 발생");
-			
+public class practice{
+	public static void main(String[] args) {	
+		
+	try {
+		FileReader fr = new FileReader("C:\\Users\\yura0\\Yellow.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		int c;
+		while((c = br.read()) != -1)
+			System.out.print((char)c);   
+	
+		
+		System.out.println("무사히 파일 가져오기 성공");
+		br.close();
+		fr.close();
+	}catch (Exception e){
+		e.printStackTrace();
 		}
 	}
-
 }
-
