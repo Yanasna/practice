f572c13dba61523943fb850a79c6e02e49b4c1a0
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-04-13 18:23:53 +0900

    4/13_1

--- react/App.jsx
+++ react/App.jsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-import Header from './W03/Header';
-import Skill from './W03/Skill';
-import Career from './W03/Career';
-import Reward from './W03/Reward';
-
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const obj_a = {
-    color : "green",
-  }
-  const obj = {a : 1}
+  const [number, setNumber] = useState(0);
+  const [light, setLight] = useState(0);
   return (
-    <div>
-      <h1>이력서</h1>
-      <h2>개인정보</h2>
-      <Header name = "김영중" phone ="010-2929-4432" email="Yura034030@gmail.com"/>
-      <Skill contents = "어느정도의 react jsx의 문법을 다룰 수 있습니다."/>
-      <Career content_1 = "학력사항" content_2 = "학력 내용"/>
-      <Career content_1 = "학력사항" content_2 = "경력 내용"/>
-      <Reward content = "none/null"/>
-      <p style = {obj_a}>{obj.a}</p>
-      <p className = "style_1">AA</p>
-    </div>
+    <>  
+      <h1>[Quiz] State</h1>
+      <h2 style = {light % 2 === 0 ?
+        {backgroundColor : "#ffcc00"} : 
+        {backgroundColor : "gray"}}>
+          {light % 2 == 0 ? "ON" : "OFF"}</h2>
+      <button onClick={() => 
+        {setLight(light + 1)}}>
+        {light % 2 == 0 ? "끄기" : "켜기" }
+      </button>
+      <h2>{number}</h2>
+      <button onClick={()=>
+        {setNumber(number+1)}}>+</button>
+    </>
   )
 }
 
-export default App
-
+export default App;
